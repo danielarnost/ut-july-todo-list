0179df9cd74a1814de645d1645398d2db3f8c87d
danielarnost <danielarnost@utexaas.edu>
2017-01-19 00:27:17 -0600

    handlebars and post route

--- app.js
+++ app.js
@@ -3,10 +3,14 @@ var PORT = process.env.PORT || 3000;
 var app = express();
 var Sequelize = require ('sequelize');
 var exphbs = require('express-handlebars');
+
 app.engine('handlebars',  exphbs({defaultLayout: 'main'}));
 app.set('view engine',  'handlebars');
 
 
+var bodyParser = require('body-parser');
+// parse application/x-www-form-urlencoded 
+app.use(bodyParser.urlencoded({ extended: false }));
 var models = require ('./models');
 // console.log(models.TodoItem);
 // var exhbs = require ('handlebars', exhbs({defaultLayout: 'main'}))
@@ -57,7 +61,15 @@ res.render('home', {tasks:data});
 
 });
 app.post('/todos/', function (req, res) {
-res.send("add a todo!");
+	console.log(req.body.newTask);
+	models.TodoItem.create({
+			task: req.body.newTask,  //hardcoding a new task?
+			done: false
+	}).then(function(data){
+			//add a new to do 
+res.redirect('/');
+	
+	});
 
 });
 
